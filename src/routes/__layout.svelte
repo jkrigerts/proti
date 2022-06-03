b8903a9b1f2e0a7dec98299ce4570d9e0a0c5d2a
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2022-06-03 17:59:38 +0300

    Initial release

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>proti.lv</title>
+</svelte:head>
+
+<slot />
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<div class="container">
+	<h1 class="greeting">Sveicināti! <em class="proti">proti.lv</em> vēl top!</h1>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;1,300&display=swap');
+
+	.container {
+		font-family: 'Josefin Sans', sans-serif;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 0;
+	}
+
+	.greeting {
+		margin: 20px;
+		text-align: center;
+	}
+
+	.proti {
+		font-style: italic;
+		font-size: 1.3em;
+		color: rgb(132, 23, 233);
+	}
+</style>
