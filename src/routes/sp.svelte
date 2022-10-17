3773d071302fc14ecdce4cffe7348c1c79183738
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2022-10-17 20:45:48 +0300

    If prezentācijai poga

--- src/routes/sp.svelte
+++ src/routes/sp.svelte
@@ -1,13 +1,37 @@
-<div>
-	<iframe title="Sarošanās jeb if" src="/if.pdf" height="400" width="700" />
+<div class="outer">
+	<div class="inner">
+		<a href="/if.pdf">if prezentācija</a>
+		<iframe title="Sarošanās jeb if" src="/if.pdf" height="400" width="700" />
+	</div>
 </div>
 
 <style>
-	div {
+	.outer {
 		width: 100vw;
 		height: 100vh;
 		display: flex;
 		align-items: center;
 		justify-content: center;
+	}
+	.inner {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	a {
+		width: 200px;
+		text-decoration: none;
+		color: white;
+		padding: 15px 20px;
+		border-radius: 3px;
+		font-weight: 600;
+		background-color: blueviolet;
+		margin-bottom: 30px;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		transition: all 0.4s;
+	}
+	a:hover {
+		background-color: blue;
 	}
 </style>
